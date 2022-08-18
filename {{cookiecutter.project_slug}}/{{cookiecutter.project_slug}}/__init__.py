ca52072841756f3c6a835382efa3d938d0592ad0
wpk <william.krekelberg@nist.gov>
2022-08-18 19:18:57 -0400

    added pyproject.toml support and conda-merge support

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -10,17 +10,26 @@ Top level stuff
    a_function - a test function
    another_func - another test fuction
 """
-import pkg_resources
+
 
 from .{{ cookiecutter.project_slug }} import a_function
 from .core import another_func
 
+
+# updated versioning scheme
 try:
-    __version__ = pkg_resources.get_distribution("{{ cookiecutter.project_slug }}").version
+    from importlib.metadata import version as _version
+except ImportError:
+    # if the fallback library is missing, we are doomed.
+    from importlib_metadata import version as _version  # type: ignore[no-redef]
+
+try:
+    __version__ = _version(("{{ cookiecutter.project_slug }}")
 except Exception:
     # Local copy or not installed with setuptools.
     # Disable minimum version checks on downstream libraries.
     __version__ = "999"
+
 
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
