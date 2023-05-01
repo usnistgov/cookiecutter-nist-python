ee3e381cbb89c8a01017cae82d9f7e5187c0839d
wpk <william.krekelberg@nist.gov>
2023-05-01 16:23:46 -0400

    chore: cleanup scripts/dist-conda.mk

--- {{cookiecutter.project_slug}}/scripts/dist-conda.mk
+++ {{cookiecutter.project_slug}}/scripts/dist-conda.mk
@@ -1,17 +1,20 @@
 package_name?={{ cookiecutter.project_name }}
 
-.PHONY: help clean recipe build command
+.PHONY: help clean-recipe clean-build recipe build command
 
 help:
 	@echo Makefile for building conda dist
-clean:
-	find . -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} +
+clean-recipe:
+	rm -rf $(package_name)
 
-recipe: clean
+clean-build:
+	rm -rf build
+
+recipe: clean-recipe
 	grayskull pypi $(package_name)     && \
 	cat $(package_name)/meta.yaml
 
-build:
+build: clean-build
 	conda mambabuild --output-folder=build --no-anaconda-upload .
 
 command:
