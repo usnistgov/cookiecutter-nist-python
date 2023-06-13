58743c1774ac9e5a2e0731bcb005cb10d99924e4
wpk <william.krekelberg@nist.gov>
2023-06-13 11:29:04 -0400

    chore: remove importlib_metadata reference

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -17,11 +17,6 @@ from .{{ cookiecutter.project_slug }} import a_function
 # updated versioning scheme
 try:
     from importlib.metadata import version as _version
-except ImportError:
-    # if the fallback library is missing, we are doomed.
-    from importlib_metadata import version as _version  # type: ignore[no-redef]
-
-try:
     __version__ = _version("{{ cookiecutter.project_slug }}")
 except Exception:
     # Local copy or not installed with setuptools.
