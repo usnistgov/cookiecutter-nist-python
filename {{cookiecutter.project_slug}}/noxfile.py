e6ac7dfcc4c6fac522df3ffc6774bacce2f45c46
wpk <william.krekelberg@nist.gov>
2023-06-29 16:54:21 -0400

    chore: update noxfile

--- {{cookiecutter.project_slug}}/noxfile.py
+++ {{cookiecutter.project_slug}}/noxfile.py
@@ -1100,7 +1100,8 @@ def _create_doc_examples_symlinks(session, clean=True):
 
             os.symlink(target_rel, link)
 
-
+# # If want seperate env for updating/reporting version with setuptools-scm
+# # We do this from dev environment.
 # # ** version report/update
 # @DEFAULT_SESSION_VENV
 # def version_scm(
