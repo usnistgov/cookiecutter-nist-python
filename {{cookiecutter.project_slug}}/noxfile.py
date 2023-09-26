3cdaff7620f4af024e9cbe244cd839a40b747977
wpk <william.krekelberg@nist.gov>
2023-09-26 16:50:13 -0400

    chore: bump pyproject2conda version to >=0.7.0

--- {{cookiecutter.project_slug}}/noxfile.py
+++ {{cookiecutter.project_slug}}/noxfile.py
@@ -243,7 +243,7 @@ def requirements(
     """
     pkg_install_venv(
         session=session,
-        reqs=["pyproject2conda>=0.6.1"],
+        reqs=["pyproject2conda>=0.7.0"],
         name="reqs",
         update=update,
         log_session=log_session,
