b1bda8405873534ef5a143e1163bfb0d7dca72bc
wpk <william.krekelberg@nist.gov>
2023-11-06 15:07:48 -0500

    chore: update cli.py file name to remove need for post_gen_project hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """Post generate hooks."""
-from pathlib import Path
-
-PROJECT_PATH = Path.cwd()
 
 
 if __name__ == "__main__":
-    if "no" in "{{ cookiecutter.command_line_interface|lower }}":
-        cli_path = PROJECT_PATH / "src" / "{{ cookiecutter.project_slug }}" / "cli.py"
-        cli_path.unlink()
+    # PROJECT_PATH = Path.cwd()
+
+    # if "no" in "{{ cookiecutter.command_line_interface|lower }}":
+    #     cli_path = PROJECT_PATH / "src" / "{{ cookiecutter.project_slug }}" / "cli.py"
+    #     cli_path.unlink()
+    pass
