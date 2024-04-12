1968ba0b57df0ffa023910ca0914887f5f780219
wpk <william.krekelberg@nist.gov>
2024-04-12 12:31:38 -0400

    chore: downgrade pyright and update pre-commit hooks

--- {{cookiecutter.project_name}}/tools/noxtools.py
+++ {{cookiecutter.project_name}}/tools/noxtools.py
@@ -214,7 +214,7 @@ def infer_requirement_path(
     # adjust filename
     filename = name
     if ext is not None and not filename.endswith(ext):
-        filename = filename + ext
+        filename += ext
     if python_version is not None:
         prefix = py_prefix(python_version)
         if not filename.startswith(prefix):
