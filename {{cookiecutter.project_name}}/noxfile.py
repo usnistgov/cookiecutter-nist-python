1df9c917a34449b91b22e33476ccc1d2f2bfb11d
wpk <william.krekelberg@nist.gov>
2024-06-21 11:00:36 -0400

    chore: bugfix in noxfile.py
    Added copy to parameters so multiple sessions have same initial parameters

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -841,8 +841,7 @@ def docs(
     runner.run_commands(opts.docs_run)
 
     cmd = opts.docs or []
-    if not opts.docs_run and not cmd:
-        cmd = ["html"]
+    cmd = ["html"] if not opts.docs_run and not cmd else list(cmd)
 
     if "symlink" in cmd:
         cmd.remove("symlink")
@@ -931,6 +930,7 @@ def typing(  # noqa: C901
     session.env["MYPY_CACHE_DIR"] = str(Path(session.create_tmp()) / ".mypy_cache")
 
     if "clean" in cmd:
+        cmd = list(cmd)
         cmd.remove("clean")
 
         for name in [".mypy_cache", ".pytype"]:
@@ -1183,6 +1183,7 @@ def conda_build(session: nox.Session, opts: SessionParams) -> None:
     if cmds is None:
         cmds = []
 
+    cmds = list(cmds)
     if "clean" in cmds:
         cmds.remove("clean")
         session.log("removing directory dist-conda/build")
