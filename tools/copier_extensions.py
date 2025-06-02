8f2aa534198ded033fc3600e55d1f4cb6e8b4249
wpk-nist-gov <wpk@nist.gov>
2025-06-02 15:35:07 -0400

    chore: temporary
    chore: temporary

--- tools/copier_extensions.py
+++ tools/copier_extensions.py
@@ -3,16 +3,57 @@
 from __future__ import annotations
 
 import datetime
+from collections.abc import Mapping
 from typing import Any
 
 from copier_templates_extensions import ContextHook
 
 
-class CookiecutterNamespace(ContextHook):  # pylint: disable=abstract-method
-    """Template hooks for copier"""
+class SmartDict(Mapping[Any, Any]):
+    """A mapping."""
 
-    def hook(self, context: dict[str, Any]) -> dict[str, Any]:  # noqa: PLR6301
-        """Hook to create other variables."""
-        context["__year"] = str(datetime.datetime.now(tz=None).year)  # noqa: DTZ005
-        context["__answers"] = context["_copier_conf"]["answers_file"]
-        return {"cookiecutter": context}
+    def __init_subclass__(cls):
+        cls._computed_keys = {}
+
+    def __init__(self, init):
+        self._init = init
+
+    @classmethod
+    def register(cls, func):
+        cls._computed_keys[func.__name__] = func
+        return func
+
+    def __getitem__(self, item):
+        if item in self._computed_keys:
+            return self._computed_keys[item](self._init)
+
+        return self._init[item]
+
+    def __contains__(self, item):
+        return item in self._init or item in self._computed_keys
+
+    def __iter__(self):
+        yield from self._init
+        yield from self._computed_keys
+
+    def __len__(self):
+        return len(self._computed_keys) + len(self._init)
+
+
+class CookiecutterContext(SmartDict):
+    pass
+
+
+@CookiecutterContext.register
+def __year(_):
+    return str(datetime.datetime.now(tz=None).year)  # noqa: DTZ005
+
+
+@CookiecutterContext.register
+def __answers(context):
+    return context["_copier_conf"]["answers_file"]
+
+
+class CookiecutterNamespace(ContextHook):
+    def hook(self, context):
+        return {"cookiecutter": CookiecutterContext(context)}
