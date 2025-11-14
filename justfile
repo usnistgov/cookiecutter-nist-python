0df2244284319f1db801133fad6e996cea1bee94
wpk-nist-gov <wpk@nist.gov>
2025-11-14 14:10:36 -0500

    chore: update hooks

--- justfile
+++ justfile
@@ -350,9 +350,10 @@ readme-pdf:
 
 update-template-pre-commit-config:
     uvx prek --cd {{ "{{cookiecutter.project_name}}" }} autoupdate
-    -uv run --no-project --script tools/requirements_lock.py --sync --upgrade requirements/pre-commit-additional-dependencies.txt
+    -uv run --no-project --script tools/requirements_lock.py \
+        --sync --upgrade requirements/pre-commit-additional-dependencies.txt
     uv run --no-project --script \
-        tools/update_pre_commit_additional_dependencies.py \
+        {{ "{{cookiecutter.project_name}}" }}/tools/update_pre_commit_additional_dependencies.py \
         -r requirements/lock/pre-commit-additional-dependencies.txt \
         {{ "{{cookiecutter.project_name}}" }}/.pre-commit-config.yaml
     uvx prek --cd {{ "{{cookiecutter.project_name}}" }} run prettier --files .pre-commit-config.yaml
--- {{cookiecutter.project_name}}/tools/update_pre_commit_additional_dependencies.py
+++ {{cookiecutter.project_name}}/tools/update_pre_commit_additional_dependencies.py
@@ -1,3 +1,5 @@
+"""Update ``additional_dependencies`` in ``.pre-commit-config.yaml``"""
+
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
@@ -6,10 +8,11 @@
 # ]
 # ///
 # NOTE: adapted from https://github.com/pre-commit/sync-pre-commit-deps
-# ruff: noqa: D100, D103, T201
+# ruff: noqa: D103, T201
 from __future__ import annotations
 
 import argparse
+from functools import lru_cache
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
@@ -20,7 +23,7 @@ if TYPE_CHECKING:
 
 
 SUPPORTED_FROM_ID = ["typos", "codespell", "ruff-format"]
-SUPPORT_TO_ID = ["doccmd", "justfile-format"]
+SUPPORT_TO_ID = ["doccmd", "justfile-format", "nbqa"]
 
 _ARGUMENT_HELP_TEMPLATE = (
     "The `{}` argument to the YAML dumper. "
@@ -30,13 +33,15 @@ _ARGUMENT_HELP_TEMPLATE = (
 
 
 def _get_versions_from_ids(
-    loaded: dict[str, Any], hook_ids_from: Container[str]
+    loaded: dict[str, Any],
+    hook_ids_from: Container[str],
 ) -> dict[str, Any]:
     versions = {}
     for repo in loaded["repos"]:
         if repo["repo"] not in {"local", "meta"}:
             for hook in repo["hooks"]:
-                if (hid := hook["id"]) in hook_ids_from:
+                hid = hook["id"]
+                if hid in hook_ids_from:
                     # `mirrors-mypy` uses versions with a 'v' prefix, so we
                     # have to strip it out to get the mypy version.
                     cleaned_rev = repo["rev"].removeprefix("v")
@@ -47,6 +52,7 @@ def _get_versions_from_ids(
     return versions
 
 
+@lru_cache
 def _get_versions_from_requirements(
     requirements_path: Path | None,
 ) -> dict[str, Any]:
@@ -62,89 +68,49 @@ def _get_versions_from_requirements(
     return versions
 
 
-def _get_versions_from_lastversion(dependencies: Sequence[str]) -> dict[str, Any]:
-    if not dependencies:
-        return {}
-
+@lru_cache
+def _get_version_from_lastversion(dep: str) -> str:
     from lastversion import latest
 
-    versions = {}
-    for dep in dependencies:
-        versions[dep] = latest(dep, output_format="tag")
-    return versions
+    return latest(dep, output_format="tag")
 
 
-def main(argv: Sequence[str] | None = None) -> int:  # noqa: PLR0914
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "path",
-        default=".pre-commit-config.yaml",
-        type=Path,
-        nargs="?",
-        help="The pre-commit config file to sync to.",
-    )
+def _get_versions_from_lastversion(dependencies: Sequence[str]) -> dict[str, Any]:
+    return {dep: _get_version_from_lastversion(dep) for dep in dependencies}
 
-    # defaults below match pre-commit config as documented
-    parser.add_argument(
-        "--yaml-mapping",
-        type=int,
-        default=2,
-        help=_ARGUMENT_HELP_TEMPLATE.format("mapping"),
-    )
-    parser.add_argument(
-        "--yaml-sequence",
-        type=int,
-        default=4,
-        help=_ARGUMENT_HELP_TEMPLATE.format("sequence"),
-    )
-    parser.add_argument(
-        "--yaml-offset",
-        type=int,
-        default=2,
-        help=_ARGUMENT_HELP_TEMPLATE.format("offset"),
-    )
-    parser.add_argument(
-        "--id",
-        dest="hook_ids_update",
-        type=str,
-        help=f"hook id's to allow update of additional_dependencies.  Defaults to {SUPPORT_TO_ID}",
-        action="append",
-        default=SUPPORT_TO_ID,
-    )
-    parser.add_argument(
-        "--from",
-        dest="hook_ids_from",
-        type=str,
-        help=f"hook id's to extract requirements from.  Defaults to {SUPPORTED_FROM_ID}",
-        action="append",
-        default=SUPPORTED_FROM_ID,
-    )
-    parser.add_argument(
-        "-l",
-        "--last",
-        dest="lastversion_dependencies",
-        type=str,
-        help="Dependency to lookup using `lastversion`.  Requires network access.",
-        action="append",
-        default=[],
-    )
-    parser.add_argument(
-        "-r",
-        "--requirements",
-        type=Path,
-        help="Requirements file to lookup pinned requirements.",
-    )
 
-    args = parser.parse_args(argv)
-    path: Path = args.path
+def _get_hook_ids(loaded: dict[str, Any]) -> list[str]:
+    out: list[str] = []
+    for repo in loaded["repos"]:
+        if repo["repo"] not in {"local", "meta"}:
+            out.extend(hook["id"] for hook in repo["hooks"])
+    return out
 
-    yaml_mapping: int = args.yaml_mapping
-    yaml_sequence: int = args.yaml_sequence
-    yaml_offset: int = args.yaml_offset
 
-    hook_ids_update: frozenset[str] = args.hook_ids_update
-    hook_ids_from: frozenset[str] = args.hook_ids_from
+def _limit_hooks(
+    hook_ids: Sequence[str],
+    use_all: bool,
+    include: Sequence[str],
+    exclude: Sequence[str],
+) -> list[str]:
+    include = hook_ids if use_all else include
+    return [x for x in include if x not in exclude]
 
+
+def _process_file(
+    path: Path,
+    yaml_mapping: int,
+    yaml_sequence: int,
+    yaml_offset: int,
+    to_all: bool,
+    to_include: Sequence[str],
+    to_exclude: Sequence[str],
+    from_all: bool,
+    from_include: Sequence[str],
+    from_exclude: Sequence[str],
+    requirements: Path | None,
+    lastversion_dependencies: Sequence[str],
+) -> int:
     yaml = ruamel.yaml.YAML()
     yaml.preserve_quotes = True
     yaml.indent(yaml_mapping, yaml_sequence, yaml_offset)
@@ -152,9 +118,17 @@ def main(argv: Sequence[str] | None = None) -> int:  # noqa: PLR0914
     with path.open(encoding="utf-8") as f:
         loaded = yaml.load(f)
 
+    hook_ids = _get_hook_ids(loaded)
+    hook_ids_update = _limit_hooks(
+        hook_ids, use_all=to_all, include=to_include, exclude=to_exclude
+    )
+    hook_ids_from = _limit_hooks(
+        hook_ids, use_all=from_all, include=from_include, exclude=from_exclude
+    )
+
     versions = _get_versions_from_ids(loaded, hook_ids_from)
-    versions.update(_get_versions_from_requirements(args.requirements))
-    versions.update(_get_versions_from_lastversion(args.lastversion_dependencies))
+    versions.update(_get_versions_from_requirements(requirements))
+    versions.update(_get_versions_from_lastversion(lastversion_dependencies))
 
     updated = []
     for repo in loaded["repos"]:
@@ -178,6 +152,113 @@ def main(argv: Sequence[str] | None = None) -> int:  # noqa: PLR0914
         return 1
 
     return 0
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "paths",
+        type=Path,
+        nargs="*",
+        help="The pre-commit config file to sync to.",
+    )
+
+    # yaml defaults are my preference
+    parser.add_argument(
+        "--yaml-mapping",
+        type=int,
+        default=2,
+        help=_ARGUMENT_HELP_TEMPLATE.format("mapping"),
+    )
+    parser.add_argument(
+        "--yaml-sequence",
+        type=int,
+        default=4,
+        help=_ARGUMENT_HELP_TEMPLATE.format("sequence"),
+    )
+    parser.add_argument(
+        "--yaml-offset",
+        type=int,
+        default=2,
+        help=_ARGUMENT_HELP_TEMPLATE.format("offset"),
+    )
+    # hook id to extract from
+    parser.add_argument(
+        "--from",
+        dest="from_include",
+        action="append",
+        default=SUPPORTED_FROM_ID,
+        help=f"hook id's to extract requirements from.  Defaults to {SUPPORTED_FROM_ID}",
+    )
+    parser.add_argument(
+        "--from-all",
+        action="store_true",
+        help="Extract dependencies from all hook id's",
+    )
+    parser.add_argument(
+        "--from-exclude",
+        action="append",
+        default=[],
+        help="Hook id's to exclude extracting from. Note that this is applied even if pass ``--from-all``",
+    )
+    # hook id's to update
+    parser.add_argument(
+        "--to",
+        dest="to_include",
+        action="append",
+        default=SUPPORT_TO_ID,
+        help=f"hook id's to allow update of additional_dependencies.  Defaults to {SUPPORT_TO_ID}",
+    )
+    parser.add_argument(
+        "--to-all",
+        action="store_true",
+        help="Update dependencies of all hooks",
+    )
+    parser.add_argument(
+        "--to-exclude",
+        action="append",
+        default=[],
+        help="Hook id's to exclude updating.  Note that this is applied even if pass ``--to-all``",
+    )
+    parser.add_argument(
+        "-r",
+        "--requirements",
+        type=Path,
+        default=None,
+        help="Requirements file to lookup pinned requirements to update.",
+    )
+    # use lastversion?
+    parser.add_argument(
+        "-l",
+        "--last",
+        dest="lastversion_dependencies",
+        type=str,
+        help="Dependency to lookup using `lastversion`.  Requires network access and `lastversion` to be installed.",
+        action="append",
+        default=[],
+    )
+
+    args = parser.parse_args(argv)
+    paths: list[Path] = args.paths or [Path(".pre-commit-config.yaml")]
+
+    out = 0
+    for path in paths:
+        out += _process_file(
+            path,
+            yaml_mapping=args.yaml_mapping,
+            yaml_sequence=args.yaml_sequence,
+            yaml_offset=args.yaml_offset,
+            to_all=args.to_all,
+            to_include=args.to_include,
+            to_exclude=args.to_exclude,
+            from_all=args.from_all,
+            from_include=args.from_include,
+            from_exclude=args.from_exclude,
+            requirements=args.requirements,
+            lastversion_dependencies=args.lastversion_dependencies,
+        )
+
+    return out
 
 
 if __name__ == "__main__":
