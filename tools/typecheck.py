d1dc7c4cf05fbfe4d41c0e385c94c4217e0a1914
wpk-nist-gov <wpk@nist.gov>
2025-12-15 14:41:44 -0500

    chore: update pre-commit hooks and tooling

--- tools/typecheck.py
+++ tools/typecheck.py
@@ -37,14 +37,14 @@ def _setup_logging(
 
 
 # * Runner --------------------------------------------------------------------
-def _uvx_run(
+def _do_run(
     *args: str,
     env: Mapping[str, str] | None = None,
     dry_run: bool = False,
 ) -> int:
     import subprocess
 
-    cleaned_args = ["uvx", *(os.fsdecode(arg) for arg in args)]
+    cleaned_args = [*(os.fsdecode(arg) for arg in args)]
     full_cmd = shlex.join(cleaned_args)
     logger.info("Running %s", full_cmd)
 
@@ -64,23 +64,26 @@ def _is_pyright_like(checker: str) -> bool:
     return checker in {"pyright", "basedpyright"}
 
 
-def _run_checker(
+def _get_python_flags(
     checker: str,
-    *args: str,
     python_version: str,
     python_executable: str,
-    constraints: list[Path],
-    dry_run: bool = False,
-) -> int:
+) -> tuple[str, ...]:
+    if checker == "pylint":
+        return ()
+
     if _is_pyright_like(checker):
         python_flag = "pythonpath"
     elif checker == "ty":
         python_flag = "python"
     elif checker == "pyrefly":
         python_flag = "python-interpreter-path"
-    else:
+    elif checker == "mypy":
         # default to mypy
         python_flag = "python-executable"
+    else:
+        msg = f"Unknown checker {checker}"
+        raise ValueError(msg)
 
     version_flag = "pythonversion" if _is_pyright_like(checker) else "python-version"
 
@@ -90,17 +93,41 @@ def _run_checker(
         # ty prefers `--python` flag pointing to environonmentf
         python_executable = str(Path(python_executable).parent.parent)
 
-    python_flags = (
+    return (
         *check_subcommand,
         f"--{python_flag}={python_executable}",
         f"--{version_flag}={python_version}",
     )
 
-    return _uvx_run(
-        *(f"--constraints={c}" for c in constraints),
-        *(["--with", "orjson"] if checker == "mypy" else []),
+
+def _run_checker(
+    checker: str,
+    *args: str,
+    python_version: str,
+    python_executable: str,
+    constraints: list[Path],
+    dry_run: bool = False,
+    use_uvx: bool = True,
+) -> int:
+    checker, *checker_args = shlex.split(checker)
+
+    python_flags = _get_python_flags(checker, python_version, python_executable)
+
+    uvx_args = (
+        (
+            "uvx",
+            *(f"--constraints={c}" for c in constraints),
+            *(["--with", "orjson"] if checker == "mypy" else []),
+        )
+        if use_uvx
+        else ()
+    )
+
+    return _do_run(
+        *uvx_args,
         checker,
         *python_flags,
+        *checker_args,
         *args,
         dry_run=dry_run,
     )
@@ -110,7 +137,7 @@ def _run_checker(
 def get_parser() -> ArgumentParser:
     """Get argparser."""
     parser = ArgumentParser(description="Run executable using uvx.")
-    parser.add_argument(
+    _ = parser.add_argument(
         "--python-executable",
         dest="python_executable",
         default=None,  # Path(sys.executable),
@@ -120,7 +147,7 @@ def get_parser() -> ArgumentParser:
         passed to `--python-executable` in mypy and `--pythonpath` in (based)pyright.
         """,
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "--python-version",
         dest="python_version",
         default=None,
@@ -131,7 +158,7 @@ def get_parser() -> ArgumentParser:
         to ``--pythonversion`` in pyright and ``--python-version`` otherwise.
         """,
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "-c",
         "--constraints",
         dest="constraints",
@@ -140,7 +167,7 @@ def get_parser() -> ArgumentParser:
         type=Path,
         help="Requirements (requirements.txt) specs for checker.  Can specify multiple times.",
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "-v",
         "--verbose",
         dest="verbosity",
@@ -148,24 +175,27 @@ def get_parser() -> ArgumentParser:
         default=0,
         help="Set verbosity level.  Pass multiple times to up level.",
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "-x",
         "--checker",
         dest="checkers",
         default=[],
         action="append",
-        choices=["mypy", "pyright", "basedpyright", "ty", "pyrefly"],
         help="Type checker to use.",
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "--allow-errors",
         action="store_true",
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "--dry-run",
         action="store_true",
     )
-    parser.add_argument(
+    _ = parser.add_argument(
+        "--no-uv",
+        action="store_true",
+    )
+    _ = parser.add_argument(
         "args",
         type=str,
         nargs="*",
@@ -200,10 +230,11 @@ def main(args: Sequence[str] | None = None) -> int:
             python_executable=python_executable,
             constraints=options.constraints,
             dry_run=options.dry_run,
+            use_uvx=not options.no_uv,
         )
 
     return 0 if options.allow_errors else code
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    raise SystemExit(main())
