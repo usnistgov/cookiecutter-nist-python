a9fdf6802dc13e682ceb791d2e9eabb103f7f570
wpk-nist-gov <wpk@nist.gov>
2026-09-08 13:03:29 -0400

    chore: use github self-repository syntax and sync setup-just with pre-commit just version

---
+++ {{cookiecutter.project_name}}/tools/get_version_from_pre_commit_config.py
@@ -0,0 +1,61 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "pyyaml>=6.0.3",
+# ]
+# ///
+"""Get repo version for repo from .pre-commit-config.yaml"""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+
+def _get_version(path: Path, url: str) -> str:
+    import yaml
+
+    with path.open("r", encoding="utf-8") as f:
+        data = yaml.safe_load(f)
+
+    for repo in data.get("repos", []):
+        if repo.get("repo") == url and "rev" in repo:
+            return repo["rev"].lstrip("v")
+
+    msg = f"Failed to find {url}"
+    raise ValueError(msg)
+
+
+def _get_args(argv: Sequence[str] | None = None) -> tuple[Path, str]:
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--config",
+        type=Path,
+        default=".pre-commit-config.yaml",
+        help="pre-commit config file",
+    )
+    parser.add_argument("repo", type=str, help="repo name to extract version for")
+
+    opts = parser.parse_args(argv)
+
+    return opts.config, opts.repo
+
+
+def main(argv: Sequence[str] | None = None) -> bool:
+    """Main function."""
+    config, url = _get_args(argv)
+
+    version = _get_version(config, url)
+
+    print(version)  # ruff: ignore[print]
+
+    return False
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
